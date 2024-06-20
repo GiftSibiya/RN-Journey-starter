076298052a39936bd75a6c264f11cf2a1c561c03
GiftSibiya <sibiya.bobo@gmail.com>
2024-06-20 19:11:10 +0200

    changes check

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import "./global.css"
 export default function App() {
   return (
     <View className='flex-1 items-center justify-center '>
-      <Text className='text-3xl text-center p-4 color-red-800'>Open up App.tsx to start working on your app!</Text>
+      <Text className='text-3xl text-center p-4 color-red-800'>Open up  to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
