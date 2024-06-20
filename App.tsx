e35e988a39ccda0b2406caa423d16f91762bc8da
GiftSibiya <sibiya.bobo@gmail.com>
2024-06-20 20:44:33 +0200

    Project Login Page

--- App.tsx
+++ App.tsx
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import "./global.css"
 
+import Login from './src/screens/auth/Login';
+import Register from './src/screens/auth/Register';
+
+import Home from './src/screens/home/Home';
+
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
+  const AuthStack = () => {
+    return (
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+          <Stack.Screen name="Register" component={Register} options={{headerShown: false}} />
+        </Stack.Navigator>
+    )
+  }
+
+  const MainStack = () => {
+    return (
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
+        </Stack.Navigator>
+    )
+  }
+
   return (
-    <View className='flex-1 items-center justify-center '>
-      <Text className='text-3xl text-center p-4 color-red-800'>Open up  to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="authStack" component={AuthStack} options={{headerShown: false}}/>
+      <Stack.Screen name="mainStack" component={MainStack} options={{headerShown: false}}/>
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ src/screens/auth/Login.tsx
@@ -0,0 +1,40 @@
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <View className='flex-1 items-center justify-center h-[300p] bg-slate-700 '>
+        <View className='h-[70%] w-[90%] bg-white rounded-xl'>
+
+            <View className='mb-[100px]'>
+                <Text className='text-3xl'> Welcome </Text>
+            </View>
+
+            <View className='flex items-center my-2'>
+                <TextInput className='border-[1px] w-[80%] h-[40px] rounded-xl p-2 mb-2' placeholder='Email'></TextInput>
+                <TextInput className='border-[1px] w-[80%] h-[40px] rounded-xl p-2 mb-2' placeholder='Password'></TextInput>
+            </View>
+
+            <View className='flex items-center mb-2'>
+                <View className='flex items-center justify-center rounded-2xl bg-slate-600 w-[200px] h-[50px]'>
+                    <TouchableOpacity>
+                        <Text className='text-white text-lg'>Login</Text>
+                    </TouchableOpacity>
+                </View>
+              
+            </View>
+
+            <View className='flex items-center '>
+                  <View className='flex flex-row'>
+                    <Text>Don't have an account </Text>
+                    <TouchableOpacity>
+                        <Text className='text-blue-600'>Sign Up</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    </View>
+  )
+}
+
+export default Login
